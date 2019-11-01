283f4177bfefbf67bdfe1981b4f4427d3d83511a
Miro Nurmela <miro.nurmela@futurice.com>
2019-11-01 14:31:01 +0200

    Prototype a stack implementation

--- src/Stack/Stack.js
+++ src/Stack/Stack.js
@@ -1,3 +1,15 @@
 import React from "react";
 
 export const StackPage = () => <div>STACKS</div>;
+
+export class Stack {
+  constructor() {
+    this.stack = [];
+  }
+  push(element) {
+    this.stack.push(element);
+  }
+  pop() {
+    this.stack.pop();
+  }
+}
