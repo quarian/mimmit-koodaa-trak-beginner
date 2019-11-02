39199f65cd40001d76600a019fe1099c13b4085b
Miro Nurmela <miro.nurmela@futurice.com>
2019-11-02 15:04:26 +0200

    Start drafting the sortinig playground

---
+++ src/Sorting/Sorting.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export const SortingPage = () => {
+  return (
+    <div>
+      <div>Text here about implementing different sorting algorithms</div>
+      <div></div>
+    </div>
+  );
+};
