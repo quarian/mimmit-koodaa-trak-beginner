775d8cbfb124b5c8ee7c5cf4e4a67f07748373c7
Miro Nurmela <miro.nurmela@futurice.com>
2019-11-04 21:01:46 +0200

    start styling work

--- src/App.js
+++ src/App.js
@@ -15,16 +15,30 @@ function App() {
           </Link>
         ))}
       </nav>
-      <Router>
-        {pages.map(page => (
-          <page.component key={page.title} path={page.path} />
-        ))}
-      </Router>
+      <main>
+        <Router>
+          {pages.map(page => (
+            <page.component key={page.title} path={page.path} />
+          ))}
+        </Router>
+      </main>
     </div>
   );
 }
 
-const Home = () => <div>HOME</div>;
+const Home = () => (
+  <div>
+    <h1>Welcome to a crash introduction to data structures and algorithms!</h1>
+    <p>
+      From the tabs above you'll find visualizations to help you write and
+      understand different algorithms with the help of simple data structures.
+    </p>
+    <p>
+      The source code is annotated with links and hints to sources on the web -
+      and remember to ask any and all questions from the instructors!
+    </p>
+  </div>
+);
 
 const pages = [
   { component: Home, title: "Home", path: "/" },
