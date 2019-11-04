19272111e97d29d6d032017c24cd370cd26d4ceb
Miro Nurmela <miro.nurmela@futurice.com>
2019-11-04 19:34:44 +0200

    Added tests for the balance algorithms

--- src/Stack/Stack.js
+++ src/Stack/Stack.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { testStack } from "./StackTests";
+import { testStack, parenthesisTestFunction } from "./StackTests";
 import { logTestResults } from "../TestFrameWork/testFunctions";
 
 export const StackPage = () => {
@@ -17,6 +17,7 @@ export const StackPage = () => {
         </button>
         {testResults.length > 0 && logTestResults(testResults)}
       </div>
+      <ParenthesisTester />
     </div>
   );
 };
@@ -59,6 +60,42 @@ const StackVisualisation = () => {
   );
 };
 
+const ParenthesisTester = () => {
+  const [testString, setTestSring] = useState("");
+  const [singleTestResult, setSingleTestResult] = useState(false);
+  const [testResults, setTestResults] = useState([]);
+  return (
+    <div>
+      <div>Test your stack with parenthesis balance algorithm</div>
+      <label htmlFor="parenthesis-input">Set your test string</label>
+      <input
+        id="parenthesis-input"
+        type="string"
+        value={testString}
+        onChange={e => setTestSring(e.target.value)}
+      />
+      <button
+        onClick={() => setSingleTestResult(checkParenthesisBalance(testString))}
+      >
+        Test your parenthesis balance function on the input above
+      </button>
+      <div>
+        {singleTestResult
+          ? `The parenthesis are balanced!`
+          : "The parenthesis are unbalanced or test not run"}
+      </div>
+      <button
+        onClick={() =>
+          setTestResults(parenthesisTestFunction(checkParenthesisBalance))
+        }
+      >
+        Run automated test on your balacne algorithm
+      </button>
+      {testResults.length > 0 && logTestResults(testResults)}
+    </div>
+  );
+};
+
 class Stack {
   constructor(stack) {
     this.stack = stack ? stack.getStack() : [];
@@ -76,3 +113,24 @@ class Stack {
     return this.stack;
   }
 }
+
+const checkParenthesisBalance = parenthesis => {
+  const stack = new Stack();
+  for (let i = 0; i < parenthesis.length; i++) {
+    const current = parenthesis[i];
+    if ((current === "{") | (current === "(") | (current === "[")) {
+      stack.push(current);
+    } else {
+      const popped = stack.pop();
+      if (
+        (popped === "(" && current !== ")") ||
+        (popped === "[" && current !== "]") ||
+        (popped === "{" && current !== "}") ||
+        !popped
+      ) {
+        return false;
+      }
+    }
+  }
+  return !stack.pop();
+};
--- src/Stack/StackTests.js
+++ src/Stack/StackTests.js
@@ -42,3 +42,74 @@ const testPop = stackClass => {
     "Stack should be empty after pushing 3 and popping 3"
   );
 };
+
+export const parenthesisTestFunction = balanceFunction => {
+  return [
+    noParenthesisTest(balanceFunction),
+    balancedParenthesisTest(balanceFunction),
+    singleParenthesisUnbalanced(balanceFunction),
+    mismatchParenthesisTest(balanceFunction),
+    tooManyOpenParenthesisTest(balanceFunction),
+    tooManyClosedParenthesisTest(balanceFunction)
+  ];
+};
+
+const noParenthesisTest = balanceFunction => {
+  const testString = "";
+  const result = balanceFunction(testString);
+  return testEquality(
+    result,
+    true,
+    `The function should detect that these parenthesis: ${testString} are balanced`
+  );
+};
+
+const balancedParenthesisTest = balanceFunction => {
+  const testString = "[{()[(){}]}]()()[[()]]";
+  const result = balanceFunction(testString);
+  return testEquality(
+    result,
+    true,
+    `The function should detect that these parenthesis: ${testString} are balanced`
+  );
+};
+
+const singleParenthesisUnbalanced = balanceFunction => {
+  const testString = "[";
+  const result = balanceFunction(testString);
+  return testEquality(
+    result,
+    false,
+    `The function should detect that these parenthesis: ${testString} are unbalanced`
+  );
+};
+
+const mismatchParenthesisTest = balanceFunction => {
+  const testString = "[)";
+  const result = balanceFunction(testString);
+  return testEquality(
+    result,
+    false,
+    `The function should detect that these parenthesis: ${testString} are unbalanced`
+  );
+};
+
+const tooManyOpenParenthesisTest = balanceFunction => {
+  const testString = "(()";
+  const result = balanceFunction(testString);
+  return testEquality(
+    result,
+    false,
+    `The function should detect that these parenthesis: ${testString} are unbalanced`
+  );
+};
+
+const tooManyClosedParenthesisTest = balanceFunction => {
+  const testString = "())";
+  const result = balanceFunction(testString);
+  return testEquality(
+    result,
+    false,
+    `The function should detect that these parenthesis: ${testString} are unbalanced`
+  );
+};
